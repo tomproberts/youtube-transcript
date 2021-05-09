1445df4d995ddd5db44b1ddd55b7d5201fa76d28
Thomas-Paul Roberts <tomproberts@outlook.com>
2021-05-09 15:02:29 +0100

    Implemented Skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="auto-width">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="YouTube Transcript Downloader"/>
+    <UrlInput/>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import UrlInput from '@/UrlInput.vue';
 
 @Options({
   components: {
     HelloWorld,
+    UrlInput
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+}
 </script>
 
 <style>
@@ -23,5 +29,11 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.auto-width {
+  width: min(700px, 70%);
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
---
+++ src/UrlInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="url-input">
+    <el-input
+        placeholder="https://youtube.com/watch?v="
+        prefix-icon="el-icon-video-play"
+        v-model="inputUrl">
+    </el-input>
+    <el-button @click="alertPls">Get</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class UrlInput extends Vue {
+  private inputUrl: string = '';
+
+  private alertPls() {
+    alert(this.inputUrl);
+  }
+}
+</script>
+
+<style>
+.url-input {
+  padding: 1rem;
+}
+
+.el-input {
+  padding-bottom: 1rem;
+}
+</style>
